5f06571a04a6dda92597305134c9b8a7f7d27667
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2019-05-26 11:22:40 -0300

    Add gerador e buscador

---
+++ Trabalho_2/gerador.h
@@ -0,0 +1,46 @@
+#ifndef GERADOR_H
+#define GERADOR_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+
+char v[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z'};
+
+char dicionario(int i){
+	return v[i];
+}
+
+char *gerador_aleatorio(int tam, int variedade){
+	char *vetor = new char[tam + 1];
+	srand (time(NULL)); /* initialize random seed: */
+	for (int i = 0; i < tam; i++){
+		vetor[i] = dicionario(rand() % variedade);
+	}
+	vetor[tam] = '\0';
+	return vetor;
+}
+
+char *gerador_pior_caso_1(int tam){
+	char *vetor = new char[tam + 1];
+	for (int i = 0; i < tam - 1; i++){
+		vetor[i] = dicionario(0);
+	}
+	vetor[tam - 1] = dicionario(1);
+	vetor[tam] = '\0';
+	return vetor;
+}
+
+char *gerador_pior_caso_2(int tam){
+	char *vetor = new char[tam + 1];
+	for (int i = 0; i < tam; i++){
+		vetor[i] = dicionario(0);
+	}
+	vetor[tam] = '\0';
+	return vetor;
+}
+
+#endif
