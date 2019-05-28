e4a8fe5edb0ecdce003dc8ead4243ea5147e8d71
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2019-05-28 16:51:48 -0300

    Mudanças no cabeçalho e add trabalho_3

--- Trabalho_1/trabalho_1.cpp
+++ Trabalho_1/trabalho_1.cpp
@@ -14,13 +14,11 @@ using namespace std;
 
 int main()
 {
-    cout << "TRABALHO 1 - QUICK SORT\n\n";
-
+    cout << "Quic Sorts\n\n";
     cout << "Forneca o numero de elementos do vetor: ";
 
-    int n = 0;
-    while (n < 1)
-        cin >> n;
+    int n;
+    cin >> n;
 
     int *v = new int[n];
     int *copia = new int[n];
@@ -28,51 +26,45 @@ int main()
     int opcao = 0;
     clock_t Ticks[2];
     
-    while (opcao < 1 || opcao > 2){
-            cout << "1 - Escrever intervalo com repeticao\n2 - Instancias de pior caso\n\n";
-            cin >> opcao;
-        }
-        if(opcao == 1 ){
-            gerar_instancia_aleatoria(v, n);
-        }else{
-            escrever_instancia(v, n);
-        }
+    cout << "1 - Escrever intervalo com repeticao\n2 - Instancias de pior caso\n\n";
+    cin >> opcao;
+    
+    if(opcao == 1)
+        gerar_instancia_aleatoria(v, n);
+    else
+        escrever_instancia(v, n);
+    
+    imprimir_vetor(v, n);
 
     while (true)
     {
         opcao = 0;
-        while (opcao < 1 || opcao > 5){
-            cout << "\nQual Quick Sort deseja executar?\n1 - Indices\n2 - Ponteiros\n";
-            cout << "3 - Memoria\n4 - Mediana\n5 - Aleatorio\n";
-            cin >> opcao;
-        }
-
+        cout << "\nQual Quick Sort deseja executar?\n1 - Indices\n2 - Ponteiros\n";
+        cout << "3 - Memoria\n4 - Mediana\n5 - Aleatorio\n";
+        cin >> opcao;
+        
         copiar_vetor(v, copia, n);
+        Ticks[0] = clock();
         switch (opcao)
         {
         case 1:
             cout << "\nQuick Sort por Indices\n";
-            Ticks[0] = clock();
             quick_sort_indices(copia, 0, n - 1);
             break;
         case 2:
             cout << "\nQuick Sort por Ponteiros\n";
-            Ticks[0] = clock();
             quick_sort_ponteiros(copia, copia + n - 1);
             break;
         case 3:
             cout << "\nQuick Sort Memoria\n";
-            Ticks[0] = clock();
             quick_sort_memoria(copia, copia + n - 1);
             break;
         case 4:
             cout << "\nQuick Sort Mediana\n";
-            Ticks[0] = clock();
             quick_sort_mediana(copia, copia + n - 1);
             break;
         case 5:
             cout << "\nQuick Sort Pivo Aleatório\n";
-            Ticks[0] = clock();
             quick_sort_pivo_aleatorio(copia, copia + n - 1);
             break;
         }
@@ -81,7 +73,7 @@ int main()
         if (verifica_ordenacao(copia, copia + n - 1))
         {
             cout << "O vetor esta ordenado.\n";
-            cout << "Tempo " << (double)(Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC << " milisegundos.\n";
+            cout << "Tempo " << (double)(Ticks[1] - Ticks[0]) / CLOCKS_PER_SEC << " segundos.\n";
         }
         else
             cout << "O vetor não está ordenado\n\n";
---
+++ Trabalho_3/trabalho_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct no_huffman{
+    char elem = '\0';
+    int freq;
+    int dir = -1;
+    int esq = -1;
+};
+
+struct no_heap{
+    int freq;
+    int indice;
+};
+
+int main() {
+
+}
+
+//g++ -Wall -Wextra -std=c++17 -pedantic -o executavel_trabalho_3 trabalho_3.cpp
