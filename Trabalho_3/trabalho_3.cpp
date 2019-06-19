c34549eed390a9ad683344d32786505485069f86
Rodrigo <rodrigo.menesesufc@gmail.com>
2019-06-19 17:25:55 -0300

    add byte tree

--- Trabalho_3/trabalho_3.cpp
+++ Trabalho_3/trabalho_3.cpp
@@ -1,147 +1,122 @@
 #include <iostream>
-#include "../Trabalho_2/gerador.h"
-#include "../Trabalho_2/buscador.h"
+#include <fstream>
 
 using namespace std;
 
-struct huffman_no{
-    char elem = '\0';
+struct Huffman{
+    unsigned int elem = '\0';
     int freq = 0;
     int dir = -1;
     int esq = -1;
 };
 
-struct heap_no{
+struct Noh{
     int freq;
     int indice;
 };
 
-void trocar(heap_no *a, heap_no *b){
-    heap_no aux;
+void trocar(Noh *a, Noh *b){
+    Noh aux;
     aux = *a;
     *a = *b;
     *b = aux;
 }
 
-void heapfy(heap_no *heap, int i, int last){
+void heapfy(Noh *heap, int i, int last){
+    Noh aux;
     while(i < last/2){
         if(heap[i].freq > heap[(2*i) + 1].freq){
-            trocar(&heap[i], &heap[(2 * i) + 1]);
+            aux.freq = heap[i].freq; aux.indice = heap[i].indice;
+            heap[i].freq = heap[(2*i) + 1].freq; heap[i].indice = heap[(2*i) + 1].indice;  
+            heap[(2*i) + 1].freq = aux.freq; heap[(2*i) + 1].indice = aux.indice;
         }
         if(heap[i].freq > heap[(2*i) + 2].freq){
-            trocar(&heap[i], &heap[(2 * i) + 2]);
+            aux.freq = heap[i].freq; aux.indice = heap[i].indice;
+            heap[i].freq = heap[(2*i) + 2].freq; heap[i].indice = heap[(2*i) + 2].indice;  
+            heap[(2*i) + 2].freq = aux.freq; heap[(2*i) + 2].indice = aux.indice;
         }
         heapfy(heap, (2*i) + 1, last);
         i = (2*i) + 2;
     }
 }
 
-
-int ocorrencias(int *saida){
-    int ocr = 0;
-    while (*saida != -1){
-        ocr++;
-        saida++;
-    }
-    return ocr;
-}
-
-int *frequencias(const char *texto, int tamanho, int variedade){
-    char *padrao = new char[2];
-    padrao[1] = '\0';
-
-    int *frequencia = new int[variedade];
-    int *saida = new int[tamanho];
-    
-    for(int i = 0; i < variedade; i++) *(frequencia + i) = 0;
-    for(int i = 0; i < tamanho; i++) *(saida + i) = -1;
-    
-    for(int i = 0; i < variedade; i++){
-        padrao[0] = v[i];
-        buscar_KMP(texto, padrao, saida);
-        frequencia[i] = ocorrencias(saida);
-        for(int i = 0; i < tamanho; i++) *(saida + i) = -1;
-    }
-    delete saida;
-    return frequencia;
-}
-
-void imprimirArvoreHuffman(huffman_no *no, int tamanho){
+void imprimirArvoreHuffman(Huffman *noh, int tamanho){
     for (int i = 0; i < tamanho; i++){
-        cout << "\nElemento: " << no[i].elem;
-        cout << "\nFrequencia: " << no[i].freq;
+        cout << "\nElemento: " << noh[i].elem;
+        cout << "\nFrequencia: " << noh[i].freq;
     }
 }
 
-void imprimirHeap(heap_no *no, int tamanho){
+void imprimirHeap(Noh *noh, int tamanho){
     for (int i = 0; i < tamanho; i++){
-        cout << "\nIndice: " << no[i].indice;
-        cout << "\nFrequencia: " << no[i].freq;
+        cout << "\nIndice: " << noh[i].indice;
+        cout << "\nFrequencia: " << noh[i].freq;
     }
 }
 
 int main() {
 
-    int tamanho = 10, variedade = 6;
-    // huffman_no *huffman_tree = new huffman_no[2*tamanho - 1];
-    // heap_no *heap_min = new heap_no[tamanho];
+    // Huffman *huffmanTree = new Huffman[2*tamanho - 1];
+    // Noh *Heap_min = new Noh[tamanho];
 
-    // 1ª gerar sequencia aleatório
     // 2ª contar a frequencia
-    // 3ª preencher n da huffman_tree e heap_min com a frequencia obtida
-    // 4ª completar a huffman_tree com a estratégia da remoção dupla no
-    // heap_min
+    // 3ª preencher n da huffmanTree e Heap_min com a frequencia obtida
+    // 4ª completar a huffmanTree com a estratégia da remoção dupla noh
+    // Heap_min
 
     // 1ª
 
-    const char *texto = gerador_aleatorio(tamanho, variedade);
-    int *frequencia = frequencias(texto, tamanho, variedade);
-    
-    cout << "Texto" << endl;
-    for (int i = 0; i < tamanho; i++) cout << *(texto + i) << " ";
-    cout << endl;
+    unsigned long long int O[256];
+    for (int i = 0; i < 256; i++) O[i] = 0;
 
-    cout << "Frequencia" << endl;
-    for (int i = 0; i < variedade; i++) cout << *(frequencia + i) << " ";
-    cout << endl;
+    ifstream e("text.txt");                                                                                                                                                                                                                                                                            
 
-    int num_elem = 0;
-    for (int i = 0; i < variedade; i++) 
-        if (*(frequencia + i) != 0)
-            num_elem++;
+    unsigned long long int b;
+    int count = 0;
 
+    while(!e.eof()){
+        b = e.get();
 
-    cout << "Num Elem: " << num_elem << endl;
+        if(O[b] == 0)
+            count++;
+        ++O[b];
+    }
 
-    huffman_no *arvore_huffman = new huffman_no[2 * num_elem - 1];
-    
+    e.close();
+
+    Huffman *huffmanTree = new Huffman[2 * count - 1];
     int j = 0;
-    for (int i = 0; i < variedade; i++){
-        if (frequencia[i] != 0){
-            arvore_huffman[j].elem = v[i];
-            arvore_huffman[j].freq = frequencia[i];
+    for (int i = 0; i < 256; i++){
+        if (O[i] != 0){
+            huffmanTree[j].elem = i;
+            huffmanTree[j].freq = O[i];
+            cout << "HuffmanTree: " << huffmanTree[j].elem << ", " << huffmanTree[j].freq << endl;
             j++;
         }
     }
 
-    // Perfeito, comportamento esperado.
-    imprimirArvoreHuffman(arvore_huffman, 2 * num_elem - 1);
+    // // Perfeito, comportamento esperado.
+    // imprimirArvoreHuffman(huffmanTree, 2 * num_elem - 1);
 
-    heap_no *heap = new heap_no[num_elem];
+    Noh *Heap = new Noh[count];
     j = 0;
-    for (int i = 0; i < variedade; i++){
-        if (frequencia[i] != 0){
-            heap[j].indice = j;
-            heap[j].freq = frequencia[i];
-            j++;
-        }
+    for (int i = 0; i < count; i++){
+        Heap[i].indice = i;
+        Heap[i].freq = huffmanTree[i].freq;
     }
 
-    // Perfeito, comportamento esperado.
-    imprimirHeap(heap, num_elem);
-    heapfy(heap, num_elem, num_elem);
-    cout << "\nHeap Após heapfy\n";
-    imprimirHeap(heap, num_elem);
+    heapfy(Heap, 0, count);
+
+    for (int i = 0; i < count; i++)
+        cout << "Heap: " << Heap[i].freq << ", " << Heap[i].indice << endl;
+
+
+
+    // // Perfeito, comportamento esperado.
+    // imprimirHeap(Heap, num_elem);
+    // Heapfy(Heap, num_elem, num_elem);
+    // cout << "\nHeap Após Heapfy\n";
+    // imprimirHeap(Heap, num_elem);
     
 }
 
